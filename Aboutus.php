b46a17a43795d79cf5971edc72feff216a2892e3
Gianna <gianna@github.com>
2018-07-09 14:03:48 +0300

    info

---
+++ Aboutus.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About - Restaurant Website </title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="Menu.css">
+	<!--[if IE 7]>
+		<link rel="stylesheet" type="text/css" href="css/ie7.css">	
+	<![endif]-->
+	<!--[if IE 6]>
+		<link rel="stylesheet" type="text/css" href="css/ie6.css">		
+	<![endif]-->
+</head>
+<body>
+    <!-- start of header -->
+	<div id="header"> 
+		<span class="signboard"></span>
+		<ul id="infos">
+			<li class="home"> 
+				<a href="HomePage.html">HOME</a> 
+			</li>
+	
+		</ul>
+
+		<a href="index.html" id="logo"></a>
+	<ul id="navigation">
+		<li><a href="Appetizer.html"><span>Snacks</span></a></li>
+			<li class="Menu.html"><a href="Menu.html"><span>Main Course</span></a></li>
+			<li><a href="Dessert.html"><span>Desserts</span></a></li>
+			<li><a href="Drinks.html"><span>Drinks</span></a></li>
+		</ul>
+		<!-- /#navigation -->
+	</div> 
+	    <!-- end of header -->
+	    
+	    <!-- start of contents --> 
+	<div id="contents"> 
+		<h2 class="aboutus"><span>About</span></h2>
+		<div id="aboutus">
+			<h3>What We offer At Tratorria</h3>
+			<p>
+				The hotel trattoriais the right choice for everyone looking for classical elegance and modern comfort. .
+			</p>
+			<h3>De Cafeteria Tratoria Location</h3>
+			<p>Arriving from the direction of ngong (A2, E61), please use the access through lang'ata road  and turn right into karen street at the first turning.
+			
+			</p>
+			<h3>Be Part of Our Community</h3>
+			<p>
+				You can join and become part of the trattoria community by liking our instagram and facebook page. You can also find out more about us on our social media pages. 
+			</p>
+			
+		</div>
+	</div> <!-- end of contents -->
+
+	<div id="footer"> <!-- start of footer -->
+		<ul class="advertise">
+			<li class="delivery">
+				<h2>Hungry? We Deliver</h2>
+				<a href="menu.html">Download our Menu</a>
+			</li>
+			<li class="event">
+				<h2>Party! Party!</h2>
+				<p>Celebrate your</p>
+				<p>Special Events with Us</p>
+			</li>
+			<li class="connect">
+				<h2>Let's Keep in Touch</h2>
+			 <a class="fb" title="Facebook"></a> 
+				<a s title="Twitter"></a>
+			</li>
+		</ul>
+		<div>
+			<ul class="navigation">
+				<li><a href="Menu.html">Home</a></li>
+				<li><a href="Reservations.html">Book an event</a></li>
+				<li><a href="Aboutus.html">About</a></li>
+				<li class="last selected"><a href="contact.html">Contact</a></li>
+			</ul>
+			<span>&copy; Copyright 2018. All Rights Reserved.</span>
+		</div>
+	</div> <!-- end of footer -->
+</body>
+</html>
